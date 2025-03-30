83f294fcb2cf42f33499ffd7bc07dae87085aa35
Danny Shen <shenfive@gmail.com>
2025-03-30 13:34:09 +0800

    簡單讀取/寫入資料

--- A252HelloRealTimeDB/ViewController.swift
+++ A252HelloRealTimeDB/ViewController.swift
@@ -6,12 +6,46 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+struct User{
+    var age : Int
+    var name : String
+}
+
 
 class ViewController: UIViewController {
+    
+    var ref:DatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Auth.auth().addStateDidChangeListener { auth, user in
+            if user == nil {
+                print("尚未登入")
+                auth.signInAnonymously()
+            }else{
+                print("已登入")
+            }
+        }
+        
+        ref = Database.database().reference().child("users").child("test")
+        
+        
+        //簡單寫入讀取
+//        ref.setValue(ServerValue.timestamp())
+        
+//        ref.observeSingleEvent(of: .value) { snapshot in
+//            let time = snapshot.value as! Double
+//            let date = Date(timeIntervalSince1970: time / 1000)
+//            print(date)
+//        }
+        
+        
+        
+        
     }
 
 
