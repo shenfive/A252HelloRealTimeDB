03a16051de119cd4d95b86eecb7f6612bceef7b5
Danny Shen <shenfive@gmail.com>
2025-03-30 11:45:37 +0800

    Initial Commit

---
+++ A252HelloRealTimeDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A252HelloRealTimeDB
+//
+//  Created by 申潤五 on 2025/3/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
