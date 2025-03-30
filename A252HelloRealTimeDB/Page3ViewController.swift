8a839fc873ae6f3aef3679d571bfc4d5009a2b0a
Danny Shen <shenfive@gmail.com>
2025-03-30 16:28:34 +0800

    Fin

--- A252HelloRealTimeDB/Page3ViewController.swift
+++ A252HelloRealTimeDB/Page3ViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 import Firebase
 
+struct Content{
+    var date:Date
+    var nickName:String
+    var content:String
+}
+
+
 
 class Page3ViewController: UIViewController {
 
@@ -17,6 +24,8 @@ class Page3ViewController: UIViewController {
     
     var nickName = ""
     var subject:SubjectData!
+    
+    var displayContent : [Content] = []
     
     var ref:DatabaseReference!
     
@@ -28,6 +37,45 @@ class Page3ViewController: UIViewController {
         print(nickName)
         title = subject.subject
         // Do any additional setup after loading the view.
+        
+        
+        theTableView.delegate = self
+        theTableView.dataSource = self
+        
+        let nib = UINib(nibName: "Page2TableViewCell", bundle: nil)
+        theTableView.register(nib, forCellReuseIdentifier: "page2TableViewCell")
+        
+        
+        //讀資料
+        ref.child(subject.key).observe(.value) { snapshot in
+            self.displayContent.removeAll()
+            snapshot.children.forEach { child in
+                if let childSnapshot = child as? DataSnapshot {
+                    var newContent = Content(date: Date(), nickName: "", content: "")
+                    if let content = childSnapshot.childSnapshot(forPath: "content").value as? String{
+                        newContent.content = content
+                    }
+                    
+                    if let nickname = childSnapshot.childSnapshot(forPath: "nickname").value as? String{
+                        newContent.nickName = nickname
+                    }
+                    
+                    if let dateInt = childSnapshot.childSnapshot(forPath: "content").value as? Int{
+                        newContent.date = Date(timeIntervalSince1970: TimeInterval(dateInt / 1000))
+                    }
+                    self.displayContent.append(newContent)
+                    
+                }
+            }
+            print(self.displayContent.count)
+            self.theTableView.reloadData()
+        }
+        
+        
+        
+        
+        
+        
     }
     
     @IBAction func newMesage(_ sender: Any) {
@@ -47,4 +95,27 @@ class Page3ViewController: UIViewController {
     }
     */
 
+}
+extension Page3ViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        displayContent.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "page2TableViewCell") as! Page2TableViewCell
+        cell.subject.text = displayContent[indexPath.row].content
+        cell.lastUser.text = displayContent[indexPath.row].nickName
+        
+        let formater = DateFormatter()
+        formater.dateFormat = "MM-dd HH:mm:ss"
+        cell.lastDate.text = formater.string(from: displayContent[indexPath.row].date)
+        
+        
+//        cell.textLabel?.text = displayList[indexPath.row].subject
+        
+        
+        
+        return cell
+    }
+    
 }
